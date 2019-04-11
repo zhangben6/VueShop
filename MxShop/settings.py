da81e6c46f5dbefed5841ca7bb302ccdd7cbe0af
zhangben6 <rapzhang97@163.com>
2019-04-11 16:53:43 +0800

    goodsapp model设计

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0,BASE_DIR)
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0,os.path.join(BASE_DIR,'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -27,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# 自定义的UserProfile models类继承auth自带的实体类
+AUTH_USER_MODEL = 'users.UserProfile'
 
 # Application definition
 
@@ -37,7 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.users'
+    'users',
+    'DjangoUeditor',
+    'goods',
+    'trade',
+    'user_operation',
+
+
 ]
 
 MIDDLEWARE = [
@@ -78,11 +88,11 @@ WSGI_APPLICATION = 'MxShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mxshop',
+        'NAME': 'vue_shop',
         'USER':'root',
         'PASSWORD':'123456',
         'HOST':'127.0.0.1',
-        'OPTIONS':{'init_command':'SET default_storage_engine=INNODB;'}
+        'OPTIONS':{'init_command':'SET default_storage_engine=INNODB;'} # 默认会创建MyIsam驱动
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,17 @@
+### Django线上生鲜超市
+
+### AuthorEmail：rapzhang97@163.com
+
+### Github:https://github.com/zhangben6
+
+***
+#### Windows虚拟环境搭建:
+- 1. python -m pip install virtualenv
+- 2. mkvirtualenv -p [本地python环境目录] [虚拟环境名称]
+- 3. 使用豆瓣源加速安装Django  
+    pip install -i https://pypi.douban.com/simple/ django=1.11
+- 3. 安装Django REST framework    
+   pip install djangorestframework
+   pip install markdown,django-filter
+- 4. 上传图片处理用到的包  
+    pip install -i https://pypi.douban.com/simple/ pillow
